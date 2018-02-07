f6ebd91877c8cae9863ef45a92663e3d663e046b
Juan Hernandez <theonlyjuan148@gmail.com>
2018-02-07 17:54:57 -0600

    Start of PHP Class

---
+++ Index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="""en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Class - Juan Hernandez</title>
+    <link rel="stylesheet" type="text/css" href="css/mystyles.css"/>
+</head>
+
+<body>
+<div class="page-wrap">
+    <header class="site-header">
+        <h1>PHP Class -- Bienbenidos a PHP Class!!</h1>
+
+    </header>
+    <div class="flex-box">
+    <nav class="main-nav">
+        <ul>
+            <li class="menu-item"><a href="/">Home</a></li>
+            <li class="menu-item"><a href="#">Loops</a></li>
+            <li class="menu-item"><a href="#">Strings</a></li>
+        </ul>
+    </nav>
+
+    <section class="main-content">
+        <div class="img"><img src/></div>
+        <h2>Welcome!!</h2>
+        <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed urna in metus imperdiet maximus.
+                Phasellus elementum mauris ut ex blandit, laoreet maximus tellus semper. Maecenas in purus eu diam
+                cursus tincidunt. Nullam nisi ex, accumsan nec lacus ut, hendrerit accumsan ante. Cras vitae urna sit
+                amet eros faucibus semper.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed urna in metus imperdiet maximus.
+                Phasellus elementum mauris ut ex blandit, laoreet maximus tellus semper. Maecenas in purus eu diam
+                cursus tincidunt. Nullam nisi ex, accumsan nec lacus ut, hendrerit accumsan ante. Cras vitae urna sit
+                amet eros faucibus semper.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed urna in metus imperdiet maximus.
+                Phasellus elementum mauris ut ex blandit, laoreet maximus tellus semper. Maecenas in purus eu diam
+                cursus tincidunt. Nullam nisi ex, accumsan nec lacus ut, hendrerit accumsan ante. Cras vitae urna sit
+                amet eros faucibus semper.</p>
+        </div>
+    </section>
+
+    <aside class="right-sidebar">Sidebar</aside>
+    </div> <!-- /Flex-Box ends here -->
+    <footer>
+        <div>Learn PHP <sup>&copy;</sup> 2018</div>
+    </footer>
+
+</div> <!-- / page-wrap -->
+
+
+</body>
+</html>
